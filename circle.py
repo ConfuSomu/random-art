ec3a1482865ae588c341502944f9fad93652d80a
ConfuSomu <emilemeekel+github@gmail.com>
2018-04-30 19:18:00 +0400

    Added more and clarified comments. Variables names have been changed for more clarification.

--- circle.py
+++ circle.py
@@ -2,55 +2,56 @@ import pygame, random, sys
 from pygame.locals import *
 from guizero import App, PushButton, Text, TextBox, Combo, Slider, yesno, info
 
-def sliUpd(b):
+def brightUpdate(b): #update circle brightness
     global brightness
     brightness = int(b)
 
 def save_prompt():
-    app.destroy()
+    app.destroy() #close the main setup window
     
     if yesno("Save","Save generated drawing?"):
-        pygame.image.save(screen, fname + "." + ftype)
+        pygame.image.save(canvas, fname + "." + ftype)
         info("Saved", "Saved drawing (app will be closed)")
         sys.exit(0)
         
     info("Close", "Press OK to close app")
     sys.exit(0)
 
-def gen():
+def read(): #read input data
     button.disable()
     button.set_text("Generating...")
     app.set_title("Generating drawing...")
     
     global x_size, y_size,fname, ftype
     
-    circles = int(circles_input.get())
+    amount = int(amount_input.get())
     fname = fname_input.get()
     ftype = ftype_input.get()
     size = int(size_input.get())
     x_size = int(x_input.get())
     y_size = int(y_input.get())
     
-    run(circles, brightness, size)
+    run(amount, brightness, size)
 
-def run(circles, brightness = 255, size = 50):
-    global screen
-    screen = pygame.display.set_mode([x_size, y_size])
+def run(amount, brightness = 255, size = 50): #generate
+    #create canvas
+    global canvas
+    canvas = pygame.display.set_mode([x_size, y_size])
     
     pygame.display.set_caption("Drawing")
-    screen.fill(white)
+    canvas.fill(white)
     
     
     def randCircle(surface):
         def randBright():
             c = ["r", "g", "b"]
             
-            prim = random.choice(c)#primary color (R, G or B)to set to 255
-            c.remove(prim) #remove it from the list to not change it by accident
+            prim = random.choice(c)#primary color (R, G or B)to set to the specified brightness. This is used to control the brightness
+            c.remove(prim) #remove it from the list to not change it again
             edit = random.choice(c) #choose the color to change to an random value (0-255)
             c.remove(edit) #only keep the remainting color
             
-            color = {prim: brightness, edit: random.randint(0, brightness), c[0]: 0} #brightness = 255 for bright colors
+            color = {prim: brightness, edit: random.randint(0, brightness), c[0]: 0} #the last color is at 0 brightness to have a secendary[?]/simple color. This should be changed to have more control over the colors.
             print(color) #debug
             print(color["r"], color["g"], color["b"]) #debug
             return [color["r"], color["g"], color["b"]]
@@ -59,15 +60,15 @@ def run(circles, brightness = 255, size = 50):
         y = random.randrange(0, y_size)#, 0.25)
         color = randBright()
         
-        pygame.draw.circle(screen, color, [x, y], size, 0)
+        pygame.draw.circle(canvas, color, [x, y], size, 0)
     
     
-    #pygame.draw.circle(screen, [111, 33, 33], [500, 500], 50, 0)
-    for i in range(0, circles):
-        randCircle(screen)
+    #draw the specified number of times
+    for i in range(0, amount):
+        randCircle(canvas)
     
     #while True:
-    pygame.display.flip()
+    pygame.display.flip() #update the display
     
     save_prompt()
     
@@ -77,8 +78,8 @@ white = (255,255,255)
 app = App(title="Abstract Art - Circle")
 
 
-Text(app, text="Number of circles:", size=16)
-circles_input = TextBox(app, text="500")
+Text(app, text="Number of amount:", size=16)
+amount_input = TextBox(app, text="500")
 
 Text(app, text="File Saving", size=16)
 
@@ -91,7 +92,7 @@ ftype_input = Combo(app, options=["png","jpeg","bmp","tga"])
 Text(app, text="Advanced", size=16)
 
 Text(app, text="Color brightness (255 is brightest):")
-Slider(app, start=0, end=255, command=sliUpd)
+Slider(app, start=0, end=255, command=brightUpdate)
 
 Text(app, text="Circle size:")
 size_input = TextBox(app, text="50")
@@ -104,5 +105,5 @@ y_input = TextBox(app, text="749")
 
 Text(app, text="NOTE: After image generation, options to save file will be present\nSizes are in pixels", size=8, color="red")
 
-button = PushButton(app, text="Generate!", command=gen)
+button = PushButton(app, text="Generate!", command=read)
 app.display()
